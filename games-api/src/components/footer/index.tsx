40e5d3e21cf137ec8408ec47e247d63df7ad4475
mikael <mikaelmuniz2001@gmail.com>
2022-04-29 14:53:25 -0300

    creating a footer

--- games-api/src/components/footer/index.tsx
+++ games-api/src/components/footer/index.tsx
@@ -1,8 +1,99 @@
 import React from 'react';
-
+import BottomNavigation from '@mui/material/BottomNavigation';
+import { Avatar, Box, Grid, List, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
 
 export default function Footer() {
-    return<>
-        Footer
+    return <>
+        <BottomNavigation sx={{ textAlign: 'center', color: 'secondary.main' }}>
+            <Grid container >
+                <Grid item xs={4}>
+                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        <h4>Apresentação</h4>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Photos" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Work" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    SDAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" />
+                        </ListItem>
+                    </List>
+                </Grid>
+                <Grid item xs={4}>
+                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        <h4>Suporte</h4>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Photos" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Work" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    SDAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" />
+                        </ListItem>
+                    </List>
+                </Grid>
+                <Grid item xs={4}>
+                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        <h4>Contato</h4>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Photos" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    ASDSAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Work" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    SDAD
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" />
+                        </ListItem>
+                    </List>
+                </Grid>
+            </Grid>
+        </BottomNavigation>
     </>
 }
