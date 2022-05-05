9d141d523343b4d23c719e2e8dfeff26c4b91005
mikael <mikaelmuniz2001@gmail.com>
2022-05-05 17:45:22 -0300

    adding icons to footer

---
+++ games-api/src/components/contentHome/index.tsx
@@ -0,0 +1,10 @@
+import Carousel from "react-material-ui-carousel";
+import { Container } from "@mui/material";
+
+export default function HomeContent() {
+    return <>
+        <Container sx={{ m: 0, p: 0, width: "100%", bgcolor: 'primary.main' }}>
+            tertew
+        </Container>
+    </>
+}
