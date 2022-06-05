4a0c65f22358314acb871f25aa8c5ba15d25a4bf
Mikael <65512888+worstp@users.noreply.github.com>
2022-06-05 08:38:44 -0300

    Update index.ts

--- backend/src/modules/_database/index.ts
+++ backend/src/modules/_database/index.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const URL = "mongodb+srv://stormy:345123@cluster0.2cdtahj.mongodb.net/?retryWrites=true&w=majority";
+const URL = process.env.MONGO_URL;
 mongoose.connect(URL)
     .catch(err => {
         throw err
