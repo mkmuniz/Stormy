fe71ecafefd08ccf49e418b1329758674916f4c5
mikael <mikaelmuniz2001@gmail.com>
2022-06-05 08:57:20 -0300

    updating

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ let port = '3001';
 
 let server = https.createServer(app);
 
-server.listen(port);
+server.listen(process.env.PORT || port);
 
 server.on('listening', () => {
     return console.log('Port listening at port 3001');
